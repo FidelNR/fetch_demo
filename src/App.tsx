5a3f7e94cf8eebfc0135d71172e5c8c569783386
Fidel_Nava <eduardonaru@hotmail.com>
2024-05-07 13:53:20 -0600

    Add errors' control and wait's text

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,32 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 
-function App() {
+const useImageURL = ()=>{
   const [imgURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos",{mode: "cors"})
-    .then((response) => response.json())
+    .then((response) => {
+     if(response.status >=400){
+      throw new Error("Error!!")
+     }
+      return response.json()
+    })
     .then((response) => setImageURL(response[0].url))
-    .catch((error) => console.error(error));
-    console.log(imgURL)
-  },   [] )
+    .catch((error) => setError(error))
+    .finally(()=>setLoading(false))
+  },   []);
+
+  return {imgURL, error, loading}
+}
+
+function App() {
+  const {imgURL, error, loading}=useImageURL();
+
+  if (loading) return <p>Page loading, please wait!!!</p>
+  if (error) return <p>A network error was encountered!!!</p>
 
   return (
     <QueryClientProvider client={queryClient}>
